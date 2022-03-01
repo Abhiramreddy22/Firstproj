adebf7a2245ce0c55119dbe1db8372c2ebbccdbc
91789 <91789@LAPTOP-8G9TEUTT>
2022-03-01 17:22:22 +0530

    Commit

---
+++ core_java/src/Example2.java
@@ -0,0 +1,19 @@
+class Rectangle
+{
+	Rectangle() // default constructor
+	{
+		System.out.println("From Constructor");
+	}
+	Rectangle(int a) // argumented or parameterized
+	{
+		System.out.println("From argumented Constructor");
+	}
+}
+public class Example2 {
+	public static void main(String[]args) {
+		Rectangle r=new Rectangle();
+		
+	}
+	}
+
+
